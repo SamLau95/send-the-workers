8e771f35114c30bdbeb90eeef5fcc80608152df0
Sam Lau <samlau95@gmail.com>
2013-12-28 09:26:42 -0800

    Initialize app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SendTheWorkers::Application.config.secret_key_base = '5c391fd1971835c3d456e12ba314c02c16d7d09624d340901e8d3d4f4582c64c495b6b26c428c527f9339b5ba1a3070226106e2e64ae2029ac841dc3081ddd3d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SendTheWorkers::Application.config.session_store :cookie_store, key: '_send-the-workers_session'
